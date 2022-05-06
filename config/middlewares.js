9a60039ab31d55988e0c13c5cee0941611f5c9f5
Stephen Wilde <kink@mandys.house>
2022-05-06 01:03:20 +0100

    fix broken logo csp

--- config/middlewares.js
+++ config/middlewares.js
@@ -13,6 +13,7 @@ module.exports = [
             "blob:",
             "dl.airtable.com",
             "res.cloudinary.com",
+            "wildepoint-backend-349002.ey.r.appspot.com",
           ],
           "media-src": [
             "'self'",
@@ -20,6 +21,7 @@ module.exports = [
             "blob:",
             "dl.airtable.com",
             "res.cloudinary.com",
+            "wildepoint-backend-349002.ey.r.appspot.com",
           ],
           upgradeInsecureRequests: null,
         },
